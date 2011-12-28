615ccbb6539c69aa018c0edeefccdc86c03f9033
JZarzuela <jzarzuela@gmail.com>
2011-12-28 14:46:06 +0100

    mas

---
+++ Java/Git_Test/src/Test.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+
+/**
+ * @author n63636
+ * 
+ */
+public class Test {
+
+    /**
+     * Static Main starting method
+     * 
+     * @param args
+     *            command line parameters
+     */
+    public static void main(String[] args) {
+        try {
+            long t1, t2;
+            System.out.println("***** TEST STARTED *****");
+            Test me = new Test();
+            t1 = System.currentTimeMillis();
+            me.doIt(args);
+            t2 = System.currentTimeMillis();
+            System.out.println("***** TEST FINISHED [" + (t2 - t1) + "]*****");
+            System.exit(1);
+        } catch (Throwable th) {
+            System.out.println("***** TEST FAILED *****");
+            th.printStackTrace(System.out);
+            System.exit(-1);
+        }
+    }
+
+    /**
+     * Similar to main method but is not static
+     * 
+     * @param args
+     *            command line parameters
+     * @throws Exception
+     *             if something fails during the execution
+     */
+    public void doIt(String[] args) throws Exception {
+        System.out.println("Hola mundo de nuevo");
+    }
+}
